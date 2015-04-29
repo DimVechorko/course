e3e06369eeaec5a813779d73cc9e2cc564db893e
Dimitri <ganerals@gmail.com>
2015-04-30 00:14:36 +0300

    task1
    FINAL

--- task1/elephant.php
+++ task1/elephant.php
@@ -4,10 +4,9 @@ $exit = "";
 $str = "";
 
 if(empty($argv[1])) { $name = "Anonymos";} else {$name = $argv[1];}
-while ($str == "") {
     echo "\033[32m$name\e[0m, купи слона \n";
     $str = trim(fgets(STDIN));
-}
+
 while ($exit != "0") {
     if (empty($str)) {echo "\033[32m$name\e[0m, купи слона \n";}else {
         echo "\033[32m$name \e[0m, каждый может сказать \033х47m\033[35m$str\e[0m, а ты купи слона \n";
