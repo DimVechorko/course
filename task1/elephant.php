969623c07900156c4f37a694a625b5adb140de87
Dimitri <ganerals@gmail.com>
2015-04-29 23:41:17 +0300

    task1
    исправлено.

--- task1/elephant.php
+++ task1/elephant.php
@@ -1,19 +1,17 @@
 #!/usr/bin/php5
 <?php
-$name = "";
 $exit = "";
 $str = "";
-$stdin = fopen('php://stdin', 'r');
-if ($name == "") {echo "Введите свое имя:";}
-$name = trim(fgets(STDIN));
-if(empty($name == true)) { $name = "Anonymos";}
+
+if(empty($argv[1])) { $name = "Anonymos";} else {$name = $argv[1];}
 while ($str == "") {
     echo "\033[32m$name\e[0m, купи слона \n";
     $str = trim(fgets(STDIN));
 }
 while ($exit != "0") {
-    echo "\033[32m$name \e[0m, каждый может сказать \033х47m\033[35m$str\e[0m, а ты купи слона \n";
+    if (empty($str)) {echo "\033[32m$name\e[0m, купи слона \n";}else {
+        echo "\033[32m$name \e[0m, каждый может сказать \033х47m\033[35m$str\e[0m, а ты купи слона \n";
+    }
     $str = trim(fgets(STDIN));
     $exit = strcasecmp($str, "exit");
 }
-?>
